feed1557778af178955c15a60f540afb5240ea3c
Randy Tate <randytatejr@gmail.com>
2019-12-09 20:21:59 -0600

    rendering some goats part II

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -9,7 +9,7 @@ class Goat extends React.Component {
         <img src={goat.imgUrl} className="card-img-top" alt="Card image cap"/>
         <div className="card-body">
           <h5 className="card-title">{goat.name}</h5>
-          <p className="card-text">S{goat.age}.</p>
+          <p className="card-text">{goat.age}</p>
           <p className="card-text">{goat.description}</p>
         </div>
       </div>
