0ffe2d6e49197452855943bbb25632d63c05d7d1
Randy Tate <randytatejr@gmail.com>
2019-12-09 20:19:49 -0600

    rendering some goats

---
+++ src/components/Goat/Goat.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Goat extends React.Component {
+  render() {
+    const { goat } = this.props;
+
+    return(
+      <div className="card col-3">
+        <img src={goat.imgUrl} className="card-img-top" alt="Card image cap"/>
+        <div className="card-body">
+          <h5 className="card-title">{goat.name}</h5>
+          <p className="card-text">S{goat.age}.</p>
+          <p className="card-text">{goat.description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Goat;
--- src/components/GoatCoral/GoatCoral.js
+++ src/components/GoatCoral/GoatCoral.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Goat from '../Goat/Goat';
 
 class GoatCoral extends React.Component {
   render() {
     const myGoats = this.props.goats;
+
+    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat}/>);
     return (
-      <div className="goatCoral">
-        Goat Coral
+      <div className="goatCoral row">
+        {goatCards}
       </div>
     );
   }
